a210e226bd2871b9c8895aead2aea546f8c08790
Taechawat Konkaew <luangta_5@hotmail.com>
2018-02-18 01:38:44 +0700

    Added part of skills section and animated when content revealed

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -11,3 +11,40 @@ function handleStickyNavbar()
 		navbar.classList.remove("fixed-top");
 	}
 }
+
+// Call the function with the following parameters:
+// onViewport(selector, classNameToBeAdded, offset, callback);
+
+// Example:
+onViewport("#about-me", "animated bounce", 500);
+onViewport('#education', '', 500, function(){
+	$('.timeline li').each(function () {
+		onViewport(this, "animated bounce", 500);
+	});
+});
+onViewport('#skills', '', 500, function(){
+	$('.col').each(function () {
+		onViewport(this, "animated bounce", 500);
+	});
+});
+
+$(function(){
+	$(".progress-bar").each(function(){
+		var each_bar_width = $(this).attr('aria-valuenow');
+		$(this).width(each_bar_width + '%');
+		// $(this).animate({
+		//     width: each_bar_width + '%'
+		// }, 1000);	
+		$(this).find('span').attr('title', each_bar_width + '%');
+
+		var width_px = Math.ceil($(this).width() / 2);
+		$(this).find('span').attr('data-offset', width_px + 'px');
+	});
+
+	$('[data-toggle="tooltip"]').tooltip({trigger: 'manual'}).tooltip('show');
+
+	$(".navbar ul li a.nav-link").click(function() {
+		var scroll_to = $(this).attr("href");
+		$('body').scrollTo(scroll_to, 1000);
+	});
+});
