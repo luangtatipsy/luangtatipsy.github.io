8594f9b30305362909236dbbe94ed1696bc8853e
Taechawat Konkaew <taechawatk@taechawatk.local>
2019-02-04 16:04:17 +0700

    Added certificate owl-carousel and magnific pop up

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -52,4 +52,29 @@ $(function(){
 		var scroll_to = $(this).attr("href");
 		$('body').scrollTo(scroll_to, 1000);
 	});
+
+	$("#certificates-slider").owlCarousel({
+            items: 3,
+            center: true,
+            margin: 20,
+            loop: true,
+            dots: true,
+          });
+          
+          $('.image-link').magnificPopup({
+            type: 'image',
+            gallery: {
+              enabled: true, // set to true to enable gallery
+
+              preload: [0,2], // read about this option in next Lazy-loading section
+
+              navigateByImgClick: true,
+
+              arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+
+              tPrev: 'Previous (Left arrow key)', // title for left button
+              tNext: 'Next (Right arrow key)', // title for right button
+              tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
+            }
+          });
 });
