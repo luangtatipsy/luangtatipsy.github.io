558d5b92cf3d2ce31371e364c5ccf95239a5e960
Taechawat Konkaew <luangta_5@hotmail.com>
2021-06-12 21:48:50 +0700

    Add index.js

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import MasterLayout from "../components/master";
+
+const IndexPage = () => {
+  return <MasterLayout title="Taechawat Konkaew" />;
+};
+
+export default IndexPage;
