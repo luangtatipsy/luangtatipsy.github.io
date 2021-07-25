a63e3faea54b4288c235824bb2f31b7c89b72558
Taechawat Konkaew <luangta_5@hotmail.com>
2021-07-25 16:47:50 +0700

    Initialize React project

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-indigo-500 text-center font-bold text-4xl">
+        Hello World
+      </h1>
+    </div>
+  );
+};
+
+export default App;
