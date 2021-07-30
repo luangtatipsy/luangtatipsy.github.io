fcd3ed51ac1b5b82e6fba747e564949a637d7588
Taechawat Konkaew <luangta_5@hotmail.com>
2021-07-30 13:45:17 +0700

    Add all components to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import Navbar from "./modules/Navbar";
 import Hero from "./modules/Hero";
+import Education from "./modules/Education";
+import Experience from "./modules/Experience";
+import Certificate from "./modules/Certificate";
+import Footer from "./modules/Footer";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Education />
+      <Experience />
+      <Certificate />
+      <Footer />
     </div>
   );
 };
